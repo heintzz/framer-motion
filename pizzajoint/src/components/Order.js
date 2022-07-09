bed56d33cbeb13a2b64ab67101260decddb67585
heintzz <mhasnanr@gmail.com>
2022-07-09 16:14:10 +0700

    done all components

---
+++ pizzajoint/src/components/Order.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useEffect } from 'react'
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw',
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      mass: 0.4, // lower -> quicker
+      damping: 8, // higher -> less oscillation
+      when: 'beforeChildren', // relation with children
+      staggerChildren: 0.4, // delay between children
+    },
+  },
+  exit: {
+    x: '-100vw',
+    transition: {
+      ease: 'easeInOut',
+    },
+  },
+}
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+}
+
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true)
+    }, 3000)
+  }, [setShowModal])
+  return (
+    <motion.div
+      className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h2>Thank you for your order :)</h2>
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default Order
